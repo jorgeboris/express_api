80baefa623e0e5fc97eec97fa28628f904d2a640
Jorge Vargas <eduardo.borisq@gmail.com>
2022-04-21 23:03:48 -0500

    Creando endpoint GET /v1/explorers

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+//Endpoint
+//routes
+//Methods HTTP: GET, POST, PUT, DELETE
+app.get('/v1/explorers', (req, res) => {
+    console.log(`GET Explorers V1 API ${new Date()}`)
+    const explorer1 = {id: 1, name: "Explorer1"}
+    const explorer2 = {id: 2, name: "Explorer2"}
+    const explorers = [explorer1, explorer2]
+    //HTTP CODE STATUS: 200
+    res.status(200).json(explorers)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
